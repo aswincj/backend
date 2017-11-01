7d329648b81450b158940a563edea9113a9ea359
aswincj <aswincjmenon@gmail.com>
2017-11-02 00:25:40 +0530

    controller

--- app/routes.js
+++ app/routes.js
@@ -1,18 +1,38 @@
 var express = require('express');
 var router = express.Router();
-
-router.get('/', function(req, res) {
-    res.send('Hello World!');
-});
+var user = require('./controllers/user');
 
 router.use(function(req, res, next) {
     // do logging
     console.log('Something is happening.');
     next(); // make sure we go to the next routes and don't stop here
 });
+router.get('/user', user.index);
+router.get('/', user.index);
 
-// test route to make sure everything is working (accessed at GET http://127.0.0.1:3000/api)
-router.get('/aswin', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
-});
+/* GET request for creating a user. NOTE This must come before routes that display user (uses id) */
+router.get('/user/create', user.user_create_get);
+
+/* POST request for creating user. */
+router.post('/user/create', user.user_create_post);
+
+/* GET request to delete user. */
+router.get('/user/:id/delete', user.user_delete_get);
+
+// POST request to delete user
+router.post('/user/:id/delete', user.user_delete_post);
+
+/* GET request to update user. */
+router.get('/user/:id/update', user.user_update_get);
+
+// POST request to update user
+router.post('/user/:id/update', user.user_update_post);
+
+/* GET request for one user. */
+router.get('/user/:id', user.user_detail);
+
+/* GET request for list of all user items. */
+router.get('/users', user.user_list);
+
+
 module.exports = router;
